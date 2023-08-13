9a4101976e04364cb6cd2cd415a4ba6edf0b9fea
elizabethgim <yujingim@naver.com>
2023-08-13 22:33:18 +0900

    add : NutrientApiApplicationTests.java

---
+++ nutrient-api/src/test/java/com/yujin/nutrientapi/NutrientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujin.nutrientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
