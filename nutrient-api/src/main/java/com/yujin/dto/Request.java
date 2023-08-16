c389212666af12b41ca22960dfdad85ce91cbe8f
elizabethgim <yujingim@naver.com>
2023-08-16 23:05:48 +0900

    update: Request, 인스턴스 추가

--- nutrient-api/src/main/java/com/yujin/dto/Request.java
+++ nutrient-api/src/main/java/com/yujin/dto/Request.java
@@ -1,5 +1,21 @@
 package com.yujin.dto;
 
 public class Request {
-
+	private int id;
+	private int food_cd;
+	private String group_name;
+	private String food_name;
+	private String research_year;
+	private String maker_name;
+	private String ref_name;
+	private int serving_size;
+	private double calorie;
+	private double carbohydrate;
+	private double protein;
+	private double province;
+	private double sugars;
+	private double salt;
+	private double cholesterol;
+	private double saturated_fatty_acids;
+	private double trans_fat;
 }
