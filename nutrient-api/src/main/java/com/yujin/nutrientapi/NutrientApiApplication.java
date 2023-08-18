67db42ddabc4856a5ea00fefd9308606c4d8f917
elizabethgim <yujingim@naver.com>
2023-08-19 00:12:23 +0900

    update: MapperScan 어노테이션 추가

--- nutrient-api/src/main/java/com/yujin/nutrientapi/NutrientApiApplication.java
+++ nutrient-api/src/main/java/com/yujin/nutrientapi/NutrientApiApplication.java
@@ -1,13 +1,14 @@
 package com.yujin.nutrientapi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value="com.yujin.mapper")
 public class NutrientApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NutrientApiApplication.class, args);
 	}
-
 }
