de75d651ef4e5d509d0a9f31b2392ee436038ffe
elizabethgim <yujingim@naver.com>
2023-08-18 21:41:26 +0900

    update: return index

--- nutrient-api/src/main/java/com/yujin/controller/NutrientController.java
+++ nutrient-api/src/main/java/com/yujin/controller/NutrientController.java
@@ -1,5 +1,13 @@
 package com.yujin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class NutrientController {
-	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+		
 }
