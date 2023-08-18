ef88f624b65c783a796f335f6920d8a6a5c08cbc
elizabethgim <yujingim@naver.com>
2023-08-19 00:15:50 +0900

    update: 생성한 mapper를 서비스와 연결

--- nutrient-api/src/main/java/com/yujin/service/NutrientService.java
+++ nutrient-api/src/main/java/com/yujin/service/NutrientService.java
@@ -1,5 +1,19 @@
 package com.yujin.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.yujin.dto.Request;
+import com.yujin.dto.Resource;
+import com.yujin.mapper.NutrientMapper;
+
+@Service
 public class NutrientService {
+	private NutrientMapper nutrientDAO;
+	
+	public List<Resource> getNutrients(Request request){
+		return nutrientDAO.getNutrientList(request);
+	}
 	
 }
