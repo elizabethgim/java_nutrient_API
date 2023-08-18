a3f7aa7a48acb81bcf4d02db9cc9d08600336798
elizabethgim <yujingim@naver.com>
2023-08-18 22:22:29 +0900

    update: resource 인자 추가

--- nutrient-api/src/main/java/com/yujin/dto/Resource.java
+++ nutrient-api/src/main/java/com/yujin/dto/Resource.java
@@ -1,5 +1,22 @@
 package com.yujin.dto;
 
 public class Resource {
-
+	private int id;
+	private int food_cd;
+	private String group_name;
+	private String food_name;
+	private String research_year;
+	private String maker_name;
+	private String ref_name;
+	private int serving_size;
+	private double calorie;
+	private double carbohydrate;
+	private double protein;
+	private double province;
+	private double sugar;
+	private double salt;
+	private double cholesterol;
+	private double saturated_fatty_acids;
+	private double trans_fat;
+	
 }
