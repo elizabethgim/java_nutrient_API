6f46ea054ac00d650a0451c4ea39418d715cebd5
elizabethgim <yujingim@naver.com>
2023-08-19 00:13:15 +0900

    update: 전체 영양 정보 가져오는 함수 getNutrientList 추가

--- nutrient-api/src/main/java/com/yujin/mapper/NutrientMapper.java
+++ nutrient-api/src/main/java/com/yujin/mapper/NutrientMapper.java
@@ -1,5 +1,15 @@
 package com.yujin.mapper;
 
-public class NutrientMapper {
+import java.util.List;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.stereotype.Component;
+
+import com.yujin.dto.Request;
+import com.yujin.dto.Resource;
+
+@Component
+@MapperScan
+public interface NutrientMapper {
+	public List<Resource> getNutrientList(Request request);
 }
